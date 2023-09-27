8e6dc72467bdcff35203a2e29618833a6f782968
archer, sean <sarcher1@go.pasadena.edu>
2023-09-26 19:07:27 -0700

    Routes, controllers, GET/POST/PUT/DELETE

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const andrewRoute = require('./routes/andrew');
+const seanRoute = require('./routes/sean');
 
 mongoose
     .connect(process.env.DB_URI, { useNewUrlParser: true })
@@ -11,7 +12,12 @@ mongoose
         const app = express();
         app.use(express.json());
         app.use("/andrew", andrewRoute);
+        app.use("/sean", seanRoute);
 
+        app.get("/", async (req, res) => {
+            res.send("Home page.");
+        });
+        
         app.listen(process.env.PORT, () => {
             console.log("Successfully connected to database!");
         })
