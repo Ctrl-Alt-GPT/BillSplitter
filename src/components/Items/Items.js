7aa702b0cb24db1bb14a03965f2c3a1d03af2555
Leonidesu <andrewjunggg@gmail.com>
2023-10-04 15:22:54 -0700

    Display a message if there is no items

--- src/components/Items/Items.js
+++ src/components/Items/Items.js
@@ -5,14 +5,18 @@ import '../../styles/Items.css';
 const Items = (props) => {
   return (
     <Card className="items">
-      {props.datas.map((item) => (
-        <Item
-          key={item.id}
-          title={item.title}
-          party={item.party}
-          amount={item.amount}
-        />
-      ))}
+      {props.datas.length <= 0 ? (
+        <div className="no-items">Add an item to get started.</div>
+      ) : (
+        props.datas.map((item) => (
+          <Item
+            key={item.id}
+            title={item.title}
+            party={item.party}
+            amount={item.amount}
+          />
+        ))
+      )}
     </Card>
   );
 };
