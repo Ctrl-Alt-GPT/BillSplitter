95899731457c4cd5b29e7bbaca910864136908b3
Leonidesu <andrewjunggg@gmail.com>
2023-10-03 17:24:06 -0700

    Add team logo to page

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -2,11 +2,21 @@ import Card from '../UI/Card';
 import Items from '../Items/Items';
 import './Board.css';
 import NewItem from '../NewItem/NewItem';
+import Image from 'next/image';
+import ctrlaltgptlogo from '../../../public/ctrlaltgptlogo-nobg.png';
 
 const Board = () => {
   return (
     <Card className="board">
-      <h1>Bill Splitter</h1>
+      <header>
+        <Image
+          src={ctrlaltgptlogo}
+          alt="Ctrl+Alt+GPT logo"
+          width={75}
+          height={75}
+        />
+        <h1>Bill Splitter</h1>
+      </header>
       <NewItem />
       <Items />
     </Card>
