35f3428b4702844cea2ce023a18f37a6557d1aa5
archer, sean <sarcher1@go.pasadena.edu>
2023-10-16 09:58:12 -0700

    Input validation for tax and tips.

--- src/components/IndividualTotals/IndividualTotals.js
+++ src/components/IndividualTotals/IndividualTotals.js
@@ -1,7 +1,6 @@
 'use client'
 import { useEffect, useState } from 'react';
 
-
 const IndividualTotals = (props) => {
 
   const [items, setItems] = useState([]);
@@ -17,17 +16,16 @@ const IndividualTotals = (props) => {
 
     var subtotal = 0;
     var memberMap = {};
+
+    // Cost per person for items ordered. 
     for (var i = 0; i < items.length; i++) {
-      // Get the string of names - convert to lowercase.
+      
       const memberString = items[i].party.toLowerCase();
-      // Split into individual names. 
       const memberArray = memberString.split(/\s*,\s*/);
-      // Get the total value of the line item. 
       const price = Number(items[i].amount);
-      // Determine the individual cost for the line item. 
       const individualCost = price / memberArray.length;
-      // Add the line item value to the grand total.
       subtotal += price;
+
       for (const person of memberArray) {
         if (memberMap.hasOwnProperty(person)) {
           memberMap[person] += individualCost;
@@ -37,16 +35,11 @@ const IndividualTotals = (props) => {
       }
     }
 
-    // Iterate through member map.
+    // Tax/tips share for each member. 
     for (const person in memberMap) {
       if (memberMap.hasOwnProperty(person)) {
-        // Get the value of each members total.
         const individualAmount = memberMap[person];
-        // Divide individual total by subtotal to get proportion.
         const proportion = individualAmount / subtotal;
-        // Multiply proportion by tax and tips and add the result to each member total.
-        // const individualTax = props.taxAndTips.tax * proportion;
-        // const individualTips = props.taxAndTips.tips * proportion;
         const individualTax = props.tax * proportion;
         const individualTips = props.tips * proportion;
         memberMap[person] += individualTax;
