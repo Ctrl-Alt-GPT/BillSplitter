6171a5ad80a38cbcf0666ca18473bf37581ce124
Andrew Jung <me@andrewjunggg.com>
2023-11-04 22:31:34 -0700

    Implement DisplayTotal component redesign

--- src/components/DisplayTotal/DisplayTotal.js
+++ src/components/DisplayTotal/DisplayTotal.js
@@ -1,26 +1,28 @@
 import '../../styles/DisplayTotal.css';
-import Card from '../UI/Card';
 import GrandTotal from './GrandTotal';
 import Subtotal from './Subtotal';
+import { Card, CardContent } from '@mui/material';
 
 const DisplayTotal = (props) => {
   const extractedItemAmounts = props.datas.map((data) => data.amount);
-  
+
   const calculateGrandTotalHandler = (grandTotal) => {
     props.calculatedGrandTotal(grandTotal);
   };
 
   return (
-    <Card className="display-total">
-      <Subtotal itemAmounts={extractedItemAmounts} />
-      <p className="display-items">Total tax: ${props.taxData}</p>
-      <p className="display-items">Total tips: ${props.tipsData}</p>
-      <GrandTotal
-        itemAmounts={extractedItemAmounts}
-        totalTax={props.taxData}
-        totalTips={props.tipsData}
-        onCalculateGrandTotal={calculateGrandTotalHandler}
-      />
+    <Card>
+      <CardContent>
+        <Subtotal itemAmounts={extractedItemAmounts} />
+        <p className="display-items">Total tax: ${props.taxData}</p>
+        <p className="display-items">Total tips: ${props.tipsData}</p>
+        <GrandTotal
+          itemAmounts={extractedItemAmounts}
+          totalTax={props.taxData}
+          totalTips={props.tipsData}
+          onCalculateGrandTotal={calculateGrandTotalHandler}
+        />
+      </CardContent>
     </Card>
   );
 };
