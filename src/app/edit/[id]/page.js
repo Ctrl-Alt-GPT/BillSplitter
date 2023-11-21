f2db9fd530054302649ec0b77abe1ee80e285737
Andrew Jung <me@andrewjunggg.com>
2023-11-20 16:30:58 -0800

    Change [id] URL to deployed backend and update dependencies

--- src/app/edit/[id]/page.js
+++ src/app/edit/[id]/page.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 import Board from '../../../components/Board/Board';
 import { Alert } from '@mui/material';
 
-const DynamicEdit = ( {params} ) => {  
+const DynamicEdit = ({ params }) => {
   const [items, setItems] = useState([]);
   const [tax, setTax] = useState(0);
   const [tips, setTips] = useState(0);
@@ -15,11 +15,10 @@ const DynamicEdit = ( {params} ) => {
   }, [params]);
 
   const searchForRecords = async () => {
+    URL = 'https://gpt-billsplitter.com:3333/sean/' + id;
 
-    URL = 'http://localhost:3333/sean/' + id;
-  
-    try { 
-      const response = await fetch(URL, {method: 'GET'});
+    try {
+      const response = await fetch(URL, { method: 'GET' });
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
@@ -35,27 +34,21 @@ const DynamicEdit = ( {params} ) => {
       const tips = data[0].tips == undefined ? 0 : data[0].tips;
       setTips(tips);
       setShowNotFound(false);
-
     } catch (error) {
       console.error('Error fetching data:', error);
       setShowNotFound(true);
     }
-  }
+  };
 
   return (
     <>
-      {showNotFound == true ? 
-        <Alert 
-          severity="warning">ID: {id} was not found.
-        </Alert> : 
-        <Board 
-          items={items} 
-          taxes={tax}
-          tipVals={tips}
-        />
-      }
+      {showNotFound == true ? (
+        <Alert severity="warning">ID: {id} was not found.</Alert>
+      ) : (
+        <Board items={items} taxes={tax} tipVals={tips} />
+      )}
     </>
-  )
+  );
 };
 
 export default DynamicEdit;
